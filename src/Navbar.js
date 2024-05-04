1a55c7c5ad95ce577aa2a6bced3e3d67f34a93f2
Cody T <crthorn11@gmail.com>
2024-05-04 09:51:28 -0400

    Added some basic links per module requirements, (About me, Portfolio, Contact, Resume of which will be placed in the navbar and used on multiple screens

---
+++ src/Navbar.js
@@ -0,0 +1,23 @@
+export default function Navbar() {
+    return (
+        <nav className="nav">
+            <a href="/" className="site-banner">
+                Cody Thorntons Portfolio
+            </a>
+            <ul>
+                <li>
+                    <a href="/Portfolio">About me</a>
+                </li>
+                <li>
+                    <a href="/about">Portfolio</a>
+                </li>
+                <li>
+                    <a href="/about">Contact</a>
+                </li>
+                <li>
+                    <a href="/about">Resume</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
